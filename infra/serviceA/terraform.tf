f84929afc3fa8007b20a5c7a96ff9019006260b1
Bojan Komazec <1876278+BojanKomazec@users.noreply.github.com>
2024-10-17 10:50:38 +0100

    feat: introduced dependabot config and added dummy terraform provider deps with versions

---
+++ infra/serviceA/terraform.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      version = "4.2.0"
+    }
+    random = {
+      version = "~> 2.1.1"
+    }
+  }
+
+  required_version = "~> 1.1.9"
+}
---
+++ infra/serviceB/terraform.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      version = "3.1.0"
+    }
+    random = {
+      version = "~> 1.0.2"
+    }
+  }
+
+  required_version = "~> 1.1.9"
+}
